5a52bb99012072f0dc2d38168bcdab7a555b0cc3
tetsuromori <tetsuromori@gmail.com>
2021-06-13 23:09:49 +0900

    add solidity code

---
+++ eth-lottery.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    function Lottery() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return uint256(keccak256(block.difficulty, now, players));
+    }
+
+    function pickWinner() public {
+        uint256 index = random() % players.length;
+        players[index].transfer(this.balance);
+    }
+}
