67c270d5055354609e572ee43a58ece1379e5e21
tetsuromori <tetsuromori@gmail.com>
2021-06-17 22:33:12 +0900

    add test: requires a minimum amount of ether to enter

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -21,9 +21,19 @@ describe('Lottery Contract', () => {
     assert.ok(lottery.options.address);
   });
 
-  it('allowws one account to enter', async () => {
+  it('allows multiple accounts to enter', async () => {
     await lottery.methods.enter().send({
       from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    await lottery.methods.enter().send({
+      from: accounts[2],
       value: web3.utils.toWei('0.02', 'ether')
     });
 
@@ -32,6 +42,21 @@ describe('Lottery Contract', () => {
     });
 
     assert.equal(accounts[0], players[0]);
-    assert.equal(1, players.length);
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3, players.length);
+  });
+
+  it('requires a minimum amount of ether to enter', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 10
+      });
+      assert(false);
+    } catch (err){
+      assert.ok(err);//check if there is an error message
+    }
+    
   });
 });
